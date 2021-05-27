1ebdb4211abc1949207725d383318e75e3be89da
elninocavalcanti10 <acn2011@outlook.com>
2021-05-27 05:21:01 -0300

    layout mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import styles from './styles/Home.module.css';
-// import { useState, useContext } from 'react';
 import { ContextResultadosProvider } from './contexts/SearchContext';
 import { Search } from './components/Search';
 import { Card } from './components/Card';
@@ -11,11 +10,13 @@ export function App() {
 
     return (
       <ContextResultadosProvider>
-        <div className={styles.container}>
-          <Search />
-          
-          <Card />
+        <div className={styles.tudo}>
+          <div className={styles.container}>
+            <Search />
+            
+            <Card />
 
+          </div>
         </div>
       </ContextResultadosProvider>
     )
